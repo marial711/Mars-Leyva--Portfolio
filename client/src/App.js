37b1c4e9f9746220bff258c0f3a311ddcb5d87b5
Mars L <marialeyva782@gmail.com>
2023-11-01 20:04:48 -0400

    Implmented React

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import NavBar from './components/NavBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import LandingPage from './pages/LandingPage';
+import AboutSection from './pages/AboutPage';
+import ContactSection from './pages/ContactPage';
+
+const App = () => {
+    return (
+        <div className="container">
+            <NavBar/> 
+            <Routes>
+            </Routes>
+
+            <LandingPage />
+            <AboutSection />
+            <ContactSection />
+        </div>
+    );
+};
 
 export default App;
