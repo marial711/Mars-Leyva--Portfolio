2a954bfe7204cc50484b793ea131a68a5af9df69
Mars L <marialeyva782@gmail.com>
2023-11-07 16:59:08 -0500

    Create About Page Layout

---
+++ client/src/pages/AboutPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import SkillCircle from "../components/SkillCircle";
+
+export default function AboutSection() {
+    return (
+      <div className="min-h-screen">
+        <div>
+          <h2>Who Am I?</h2>
+          <p>
+            Culpa elit reprehenderit ad ut ad in laboris. Excepteur ipsum ad
+            veniam duis culpa laborum. Sit enim aliqua et nostrud excepteur sunt
+            aliquip pariatur proident consectetur ea consequat. Do ea ex
+            voluptate excepteur adipisicing.
+          </p>
+        </div>
+    );
+}
