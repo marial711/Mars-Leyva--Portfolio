3c00e8c5fbdaa8ca677e5ffb21b8f5a7481083e0
Mars L <marialeyva782@gmail.com>
2023-11-07 16:58:53 -0500

    Created Contact / Contact Form layout

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -5,13 +5,13 @@ export default function ContactForm() {
           <label for="visitor-name" className="form-label">
             Name
           </label>
-          <input type="text" id="visitor-name" className="form-control" />
+          <input type="text" name="visitor-name" className="form-control" />
         </div>
         <div className="mb-3 col-6">
           <label for="visitor-email" className="form-label">
             Email
           </label>
-          <input type="text" id="visitor-email" className="form-control" />
+          <input type="text" name="visitor-email" className="form-control" />
         </div>
         <div className="mb-3">
           <label for="visitor-msg" className="form-label">
@@ -21,7 +21,7 @@ export default function ContactForm() {
             placeholder="What can I help you with..."
             type="text"
             className="form-control"
-            id="visitor-msg"
+            name="visitor-msg"
             rows="4"
           ></textarea>
         </div>
--- client/src/pages/ContactPage.js
+++ client/src/pages/ContactPage.js
@@ -2,12 +2,17 @@ import ContactForm from '../components/ContactForm';
 
 export default function ContactSection() {
     return (
-    <div id="contact-section" className="min-vh-75">
-        <h2>Contact</h2>
-        <p>Want to know more information? <br />Fill out the form below and let me know how I can help  you?</p>
-        <div className="d-flex justify-content-center flex-column align-items-center">
-            <ContactForm />
+      <div className="min-h-screen">
+        <div >
+          <h2>Contact</h2>
+          <p>
+            Want to know more information? <br />
+            Fill out the form below and let me know how I can help you?
+          </p>
         </div>
-    </div>
-    )   
+        <div className="d-flex justify-content-center flex-column align-items-center">
+          <ContactForm />
+        </div>
+      </div>
+    );   
 }
