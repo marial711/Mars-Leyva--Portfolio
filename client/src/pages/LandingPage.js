26e718a898ad4cb855f4b604a03d652c201dd7e2
Mars L <marialeyva782@gmail.com>
2023-11-08 16:07:46 -0500

    Create Landing Page Layout

---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function LandingPage() {
+    return (
+      <div className="min-h-screen flex flex-col items-center justify-center">
+        <h1>Hey There, My name is Maria Leyva!</h1>
+        <p>Welcome to my Website!</p>
+      </div>
+    );
+}
