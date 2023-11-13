8af0e3f94eb28814fcdd20bf71960197f6cb9c1c
Mars L <marialeyva782@gmail.com>
2023-11-13 15:45:43 -0500

    Created Contact Layout

--- client/src/pages/ContactPage.js
+++ client/src/pages/ContactPage.js
@@ -2,17 +2,17 @@ import ContactForm from '../components/ContactForm';
 
 export default function ContactSection() {
     return (
-      <div className="min-vh-100 d-flex flex-column justify-content-around">
-        <div>
-          <h2>Contact</h2>
-          <p>
-            Want to know more information? <br />
-            Fill out the form below and let me know how I can help you?
-          </p>
-        </div>
-        <div className="d-flex justify-content-center flex-column align-items-center">
-          <ContactForm />
-        </div>
+      <div className="contact-layout">
+          <div className="contact-card">
+            <div>
+            <h2>Get In Touch</h2>
+              <p>Interested in Collaborating?</p> 
+              <p>Have any Questions, Comments, Inputs?</p>
+              <p> Fill out the form below and let me know how I can help you? </p>
+            <ContactForm />
+            </div>
+            <div className="contact-bg"></div>
+          </div>
       </div>
     );   
 }
