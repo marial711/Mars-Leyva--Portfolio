65270a1844f20454e49bae15b507ce8cdea7e68a
Mars L <marialeyva782@gmail.com>
2023-11-13 07:54:47 -0500

    Added About Section Content

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -16,6 +16,27 @@ export default function AboutSection() {
         <div>
            {/* Enter Skills Animation */}
         </div>
+        <div className="container about-section ">
+          <div className="row ">
+            <div className="col d-flex  flex-column justify-content-center">
+              <h2>My Journey</h2>
+              <p>
+                As a Web Design major at the University of Hartford, my journey
+                blends passion and curiosity. Despite being at the entry level,
+                I've immersed myself in the dynamic world of web development.
+                Collaborating on projects with friends and diving into impactful
+                internships have enriched my understanding. Beyond the
+                classroom, self-taught projects have further honed my skills.
+              </p>
+              <p>
+                Eager to contribute my evolving expertise, I stand on the cusp
+                of a professional journey. Academic knowledge, hands-on
+                experiences, and an unwavering commitment to excellence define
+                my approach. Ready to make meaningful contributions in the
+                vibrant world of web development, I see every line of code as an
+                opportunity to turn ideas into reality.
+              </p>
+            </div>
         </div>
     );
 }
