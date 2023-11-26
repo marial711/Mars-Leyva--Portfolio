efd860c8ce4f9a99c283f11d48c51c0bb977aa5e
Mars L <marialeyva782@gmail.com>
2023-11-26 14:34:45 -0500

    Create Card Flip Animation

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,6 +1,20 @@
 export default function SkillsSection(){
     return (
         <>
+            <div className="card">  {/* flip-card */}
+                <div className="card-content">   {/* flip-card-inner */}
+                    <div className="card-front"> 
+                        <p>
+                            Front
+                        </p>
+                    </div>
+                    <div className="card-back">
+                        <p>
+                            Back
+                        </p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
