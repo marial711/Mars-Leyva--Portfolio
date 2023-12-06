b7ef9a77546f65ca5ec01d16082e7aad2e8c32db
Mars L <marialeyva782@gmail.com>
2023-12-06 08:44:34 -0500

    Created Landing Page Circle Animations

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,10 +1,27 @@
 import React from "react";
+let spanishIntros = ["Hola", "Que Tal", "Buenos Dias", "Bienvenidos"]
+let englishIntros = ["Hi there!", "Good Morning", "Welcome!", "Whats up!"]
 
 export default function LandingPage() {
     return (
-      <div className="display-5 min-vh-100 d-flex flex-column align-items-center justify-content-center ">
-        <p>Hey There, My name is Maria Leyva!</p> 
-        <p>Welcome to my Website!</p>
-      </div>
+        <div className="landing-layout">
+            {/* <div className="landing-header">
+                <h1>{spanishIntros[3]}</h1>
+            </div>
+            <div>
+                <h2>I'm Maria <span>Mars</span> Leyva</h2> 
+            </div>
+            <div>            
+                <p>Part-time Coffee Enthsuiast aspiring to be a Full-Time Web Developer</p>
+            </div>             */}
+            <div className="animated-bg">
+                <div className="circle circle1"></div>
+                <div className="circle circle2"></div>
+                <div className="circle circle3"></div>
+                <div className="circle circle4"></div>
+                <div className="circle circle5"></div>
+                <div className="circle circle6"></div>
+            </div>
+        </div>
     );
 }
