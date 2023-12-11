582587ec9bfc97dba943d6ad72a60c31671a80ad
Mars L <marialeyva782@gmail.com>
2023-12-10 23:04:00 -0500

    Create AnimatedBackground.js

---
+++ client/src/components/AnimatedBackground.js
@@ -0,0 +1,12 @@
+export default function AnimatedBackground() {
+    return (
+        <div className="animated-circles">
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+            <div className="circle"></div>
+        </div>
+    )
+}
