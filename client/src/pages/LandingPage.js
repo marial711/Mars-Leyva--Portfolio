5473e6da23415bc7b4f242094d119b63cc0d4f33
Mars L <marialeyva782@gmail.com>
2023-12-20 14:07:54 -0500

    Created Typing Animation

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,10 +1,17 @@
 import React from "react";
-let spanishIntros = ["Hola", "Que Tal", "Buenos Dias", "Bienvenidos"]
-let englishIntros = ["Hi there!", "Good Morning", "Welcome!", "Whats up!"]
+import MarsLogo from "../assets/images/logo.png";
+// import { MouseMovingCircles } from "../components/AnimatedBackground";
 
 export default function LandingPage() {
     return (
         <div className="landing-layout" id="home" >
+            <div className="landing-phrase">
+                <p className="greeting">Hi there!</p>
+                <span>I am</span>
+                <h2 className="typing">Mars Leyva !</h2>
+                <p className="intro"> I like making quality handcraft beverages, and <strong>responsive web apps</strong>!</p>
+            </div>                    
+            <img src={MarsLogo} width="auto" height="500px" alt="Mars Logo" />
         </div>
     );
 }
