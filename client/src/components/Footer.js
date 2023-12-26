b8db8ff39b51b7ff75e2a4c82573e9ca96c3aa08
Mars L <marialeyva782@gmail.com>
2023-12-26 12:53:58 -0500

    Responsive footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,10 +4,10 @@ import CopyRight from "../assets/svg/copyright";
 export default function Footer () {
     return (
       <div className="footer-layout container-fluid row">
-        <div className="fs-5 col-lg-6 col-md-6 col-sm-12 footer-section d-flex align-items-center justify-content-center gap-2">
+        <div className="fs-5 col-lg-5 col-md-5 col-sm-12 footer-section d-flex align-items-center justify-content-center gap-2">
          <p>Copyright <CopyRight /> 2023 Mars Leyva</p> 
         </div>
-        <div className=" col-lg-6 col-md-6 col-sm-12 social-media-space">
+        <div className=" col-lg-5 col-md-5 col-sm-12 social-media-space">
           <a href="https://github.com/marial711">
             <GitHubLogo />
           </a>
