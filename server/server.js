6febac7344f21b64ff7d42963998f51565d0b4f1
Mars L <marialeyva782@gmail.com>
2024-01-03 10:24:09 -0500

    Created connection to Email

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 const express = require(`express`);
 const app = express();
 const cors = require(`cors`);
-
 require(`dotenv`).config({ path: `./config.env`});
 
 const port = process.env.PORT || 8000;
+const router = express.Router();
+
 app.use(cors());
 app.use(express.json());
-app.use(require(`./routes/user`));
+app.use("/", router);
 
 // DATABASE CONNECTION
 const dbo = require(`./database/conn`);
@@ -16,4 +17,22 @@ app.listen(port, () => {
         if(err) console.error(err);
     });
     console.log(`Server is running on port: ${port}!`);
+});
+
+// EMAIL CONNECTION
+const nodemailer = require("nodemailer");
+const contactEmail = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+    },
+});
+
+contactEmail.verify((error) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log("Ready to Send");
+    }
 });
